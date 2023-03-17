3922e506796172cf83c623f7c42d6bb5113daa75
yixianfeng <yixianfeng@bytedance.com>
2023-03-17 17:33:17 +0800

    add calculator demo

--- calcite-parser/javacc-demo/src/main/java/com/github/pioneeryi/Main.java
+++ calcite-parser/javacc-demo/src/main/java/com/github/pioneeryi/Main.java
@@ -1,7 +1,10 @@
 package com.github.pioneeryi;
 
+import com.github.pioneeryi.gen.Calculator;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)throws Exception {
+        Calculator parser = new Calculator(System.in);
+        parser.Start(System.out);
     }
 }
