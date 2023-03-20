927d675c5c8e3ff447d7a935f0052dd6d1175ab1
yixianfeng <yixianfeng@bytedance.com>
2023-03-20 20:51:38 +0800

    add sql parser demo

--- calcite-parser/sql-parser/src/test/java/com/github/pioneeryi/SqlParserTest.java
+++ calcite-parser/sql-parser/src/test/java/com/github/pioneeryi/SqlParserTest.java
@@ -1,3 +1,37 @@
 package com.github.pioneeryi;
+
+import com.github.pioneeryi.gen.SqlParserImpl;
+import org.apache.calcite.avatica.util.Casing;
+import org.apache.calcite.avatica.util.Quoting;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
+import org.apache.calcite.tools.FrameworkConfig;
+import org.apache.calcite.tools.Frameworks;
+import org.junit.Test;
+
 public class SqlParserTest {
+
+    @Test
+    public void testSubmitWrongOK() {
+        String sql = "select * from t_story_info";
+        final FrameworkConfig config = Frameworks.newConfigBuilder()
+                .parserConfig(SqlParser.configBuilder()
+                        .setParserFactory(SqlParserImpl.FACTORY)
+                        .setCaseSensitive(false)
+                        .setQuoting(Quoting.BACK_TICK)
+                        .setQuotedCasing(Casing.TO_UPPER)
+                        .setUnquotedCasing(Casing.TO_UPPER)
+                        .build())
+                .build();
+        SqlParser parser = SqlParser.create(sql, config.getParserConfig());
+
+        try {
+            SqlNode sqlNode = parser.parseStmt();
+            System.out.println(sqlNode.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
